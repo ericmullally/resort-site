57120ff04e7a7926604c0743eecfbac97784897f
eric mullally <ericmullally@yahoo.com>
2021-07-28 19:54:04 -0500

    added several photos,  hero section, and  logo svg. updated theme colors.

--- resort/views.py
+++ resort/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "resort/index.html")
+
+def booking(request):
+    pass
+
+def ammenities(request , slug):
+    pass
